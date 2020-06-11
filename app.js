e40386677852b5b49a1d4e2980b9764a98fc15f8
hayden7913 <hayden321@gmail.com>
2020-06-10 21:55:41 -0700

    commiting for deployment

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-
 const BASE_URL = 'https://dog.ceo/api/breeds/image/random';
 
 const fetchRandDogs = (numDogs) => axios(`${BASE_URL}/${numDogs}`);
+const renderImg = (imgUrl) => $(`<img src=${imgUrl} class="dog-image">`);
+const renderDogs = (dogImgUrls) => dogImgUrls.map(url => renderImg(url));
 
 $(async () => {
-  $(document).on('input', '#slider', function() {
-    $('#slider_value').html( $(this).val() );
+  $(document).on('input', '#slider', function () {
+    $('#slider_value').html($(this).val());
   });
 
   $('#js-dogs-form').submit(async function (event) {
     event.preventDefault();
-    const numDogsInputVal =  $('#slider').val();
+    const numDogsInputVal = $('#slider').val();
     const {data} = await fetchRandDogs(numDogsInputVal);
-    const dogImageUrls = data.message;
-    console.log(dogImageUrls)
-    });
-
-
+    const dogImages = renderDogs(data.message);
+    $('.js-dog-list').html(dogImages)
+  });
 });
 
 
